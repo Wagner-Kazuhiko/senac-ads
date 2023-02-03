719817e53ad600c2d37bb515c0299e9b54ff2362
Wagner-Kazuhiko <kazu_hiko@hotmail.com>
2023-02-02 21:12:53 -0300

    add unidade 01 aula 02 java encapsulamento

---
+++ 3-periodo/1-trimestre/orientacao-objetos-senac-intellij/src/com/kazu/unidade1/aula2/Principal.java
@@ -0,0 +1,17 @@
+package com.kazu.unidade1.aula2;
+
+public class Principal {
+    public static void main(String[] args) {
+
+        Pessoa p = new Pessoa();
+
+        p.setNome("Albert");
+        p.setCpf(111222333);
+        p.setEmail("albert@email.com");
+
+        System.out.println("O CPF inserido foi: " + p.getCpf());
+        System.out.println("O nome inserido foi: " + p.getNome());
+        System.out.println("O email inserido foi: " + p.getEmail());
+        
+    }
+}
