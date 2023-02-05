da64eaacb998bf8047abc82f44f6f5e8ccd75610
Wagner-Kazuhiko <kazu_hiko@hotmail.com>
2023-02-05 13:28:37 -0300

    heranca

---
+++ 3-periodo/1-trimestre/orientacao-objetos-senac-intellij/src/com/kazu/unidade3/aula1/Colaborador.java
@@ -0,0 +1,33 @@
+package com.kazu.unidade3.aula1;
+
+public class Colaborador {
+    private String nome;
+    private String departamento;
+
+    public Colaborador(String nome, String departamento) {
+        this.nome = nome;
+        this.departamento = departamento;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("Colaborador: %s \nDepartamento: %s",
+                this.nome, this.departamento);
+    }
+}
